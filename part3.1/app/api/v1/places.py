62a5c609a83c72efd4903ff0fc69ab2f27f24522
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 13:24:31 -0400

    subiendo places

--- part3.1/app/api/v1/places.py
+++ part3.1/app/api/v1/places.py
@@ -18,6 +18,7 @@ user_model = api.model('PlaceUser', {
     'email': fields.String(description='Email of the owner')
 })
 
+
 review_model = api.model('PlaceReview', {
     'id': fields.String(description='Review ID'),
     'text': fields.String(description='Text of the review'),
@@ -32,40 +33,75 @@ place_model = api.model('Place', {
     'latitude': fields.Float(required=True, description='Latitude of the place'),
     'longitude': fields.Float(required=True, description='Longitude of the place'),
     'owner_id': fields.String(required=True, description='ID of the owner'),
-    'owner': fields.Nested(user_model, description='Owner of the place'),
-    'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
-    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
+    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")  # friday 15
 })
+
+create_place_model = api.model('PlaceCreate', {
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner')
+})
+
 
 @api.route('/')
 class PlaceList(Resource):
     @jwt_required()
-    @api.expect(place_model)
+    @api.expect(create_place_model)
     @api.response(201, 'Place successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
         current_user = get_jwt_identity()
         place_data = api.payload
-        place_data['owner_id'] = current_user
-        
+
+        place_data['owner_id'] = current_user.get("id")
         try:
             new_place = facade.create_place(place_data)
-            return {
+
+            response_data = {
                 'id': new_place.id,
                 'title': new_place.title,
                 'description': new_place.description,
                 'price': new_place.price,
                 'latitude': new_place.latitude,
                 'longitude': new_place.longitude,
-                'owner_id': new_place.owner_id
-            }, 201
-        except ValueError as e:
-            return {'error': str(e)}, 400
+                'owner': new_place.owner_id,
+                'created_at': new_place.created_at.isoformat(),
+                'updated_at': new_place.updated_at.isoformat()
+            }
+
+            return response_data, 201
+        except Exception as e:
+            api.abort(400, str(e))
 
     @api.response(200, 'List of places retrieved successfully')
     def get(self):
-        places = facade.get_all_places()
-        return places, 200
+        try:
+            places = facade.get_all_places()
+
+            places_data = []
+            for place in places:
+
+                place_data = {
+                    'id': place.id,
+                    'title': place.title,
+                    'description': place.description,
+                    'price': place.price,
+                    'latitude': place.latitude,
+                    'longitude': place.longitude,
+                    'owner': place.owner_id,
+                    'created_at': place.created_at.isoformat(),
+                    'updated_at': place.updated_at.isoformat()
+                }
+
+                places_data.append(place_data)
+
+            return places_data, 200
+        except Exception as e:
+            api.abort(400, str(e))
+
 
 @api.route('/<place_id>')
 class PlaceResource(Resource):
@@ -74,28 +110,53 @@ class PlaceResource(Resource):
     def get(self, place_id):
         try:
             place = facade.get_place(place_id)
-            return place, 200
-        except ValueError as e:
-            return {'error': str(e)}, 404
+            if not place:
+                return {'error': 'Place not found'}, 404
+
+            place_data = {
+                'id': place.id,
+                'title': place.title,
+                'description': place.description,
+                'price': place.price,
+                'latitude': place.latitude,
+                'longitude': place.longitude,
+                'owner': place.owner_id,
+                'created_at': place.created_at.isoformat(),
+                'updated_at': place.updated_at.isoformat()
+            }
+
+            return place_data, 200
+        except Exception as e:
+            api.abort(400, str(e))
 
     @jwt_required()
-    @api.expect(place_model)
+    @api.expect(create_place_model)
     @api.response(200, 'Place updated successfully')
-    @api.response(403, 'Unauthorized action')
     @api.response(404, 'Place not found')
     @api.response(400, 'Invalid input data')
     def put(self, place_id):
         current_user = get_jwt_identity()
         place_data = api.payload
-
         try:
-            place = facade.get_place(place_id)
-            if place['owner']['id'] != current_user:
-                return {'error': 'Unauthorized action'}, 403
-            
             updated_place = facade.update_place(place_id, place_data)
-            return {
-                'message': 'Place updated successfully'
-            }, 200
-        except ValueError as e:
+            if not updated_place:
+                return {'error': 'Place not found'}, 404
+
+            if updated_place.user_id.id != current_user.get("id"):
+                return {'error': 'Unauthorized action.'}, 403
+
+            response_data = {
+                'id': updated_place.id,
+                'title': updated_place.title,
+                'description': updated_place.description,
+                'price': updated_place.price,
+                'latitude': updated_place.latitude,
+                'longitude': updated_place.longitude,
+                'owner': updated_place.owner_id,
+                'created_at': updated_place.created_at.isoformat(),
+                'updated_at': updated_place.updated_at.isoformat()
+            }
+
+            return response_data, 200
+        except Exception as e:
             return {'error': str(e)}, 400
