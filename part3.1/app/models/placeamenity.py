9ea351046de8240b411e98888b8514727193ee92
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 14:03:29 -0400

    subiendo place amenity

---
+++ part3.1/app/models/placeamenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, String, ForeignKey
+from app.models.base import BaseModel
+
+
+class PlaceAmenity(BaseModel):
+    __tablename__ = 'place_amenity'
+    __table_args__ = {'extend_existing': True}
+    place_id = Column(String, ForeignKey('place.id'), nullable=False)
+    amenity_id = Column(String, ForeignKey('place_amenity.id'), nullable=False)
+
+    def __init__(self, place_id, amenity_id):
+        super().__init__()
+        self.place_id = place_id
+        self.amenity_id = amenity_id
