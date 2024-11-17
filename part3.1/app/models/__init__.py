6aa6c56d45a3387308708eff55a253806a40e8dc
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 13:55:07 -0400

    subiendo init

---
+++ part3.1/app/models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
+
+__all__ = ["User", "Place", "Review", "Amenity"]
