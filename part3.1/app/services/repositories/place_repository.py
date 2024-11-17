75f8d76c6bbc14274524a09c9e571e083e4bdcb5
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 14:51:45 -0400

    subiendo place

---
+++ part3.1/app/services/repositories/place_repository.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from app.models.place import Place
+from app.persistence.repository import SQLAlchemyRepository
+
+
+class PlaceRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Place)
