7411829c9f36f30f74616c48a3630d2b05f9557d
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 14:06:04 -0400

    subiendo user.py

--- part3.1/app/models/user.py
+++ part3.1/app/models/user.py
@@ -1,55 +1,54 @@
 #!/usr/bin/python3
 
 from app.models.base import BaseModel
-import re
 from app import bcrypt, db
 
+
 class User(BaseModel):
-    __tablename__ = 'users'
-    
+    __tablename__ = 'user'
+    __table_args__ = {'extend_existing': True}
+
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    
-    def __init__(self, first_name: str, last_name: str, email: str, password: str, is_admin: bool = False):
+
+    def __init__(self, first_name, last_name, email, is_admin=False, password=None):
         super().__init__()
-        self.first_name = self.validate_name(first_name)
-        self.last_name = self.validate_name(last_name)
-        self.email = self.validate_email(email)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
         self.is_admin = is_admin
-        # self.password = None
-        self.hash_password(password)
-    
+        if password:
+            self.hash_password(password)
+
+        self.validate_name()
+
+    def validate_name(self):
+        if not self.first_name or len(self.first_name) > 50:
+            raise ValueError('First name must be less than 50 characters.')
+        if not self.last_name or len(self.last_name) > 50:
+            raise ValueError('Last name must be less than 50 characters.')
+
     def hash_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
-        
+
     def verify_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
 
-    @staticmethod
-    def validate_name(name: str) -> str:
-        if not name or len(name) > 50:
-            raise ValueError("Name is required and/or maximum length is of 50 characters")
-        return (name)
-    
-    @staticmethod
-    def validate_email(email : str) -> str:
-        email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-        if not re.match(email_regex, email):
-            raise ValueError("Email is required and/or format is invalid")
-        return (email)
-    
-    def update(self, first_name=None, last_name= None, email= None, password=None, is_admin= None):
+    def update(self, first_name: str = None, last_name: str = None, email: str = None, is_admin: bool = None):
         if first_name:
-            self.first_name = self.validate_name(first_name)
+            self.first_name = first_name
+            self.validate_name()
         if last_name:
-            self.last_name = self.validate_name(last_name)
+            self.last_name = last_name
+            self.validate_name()
         if email:
-            self.email = self.validate_email(email)
-        if password:
-            self.hash_password(password)
+            self.email = email
+            self.validate_email()
+
         if is_admin is not None:
             self.is_admin = is_admin
-        super().update()
+
+        self.save()
