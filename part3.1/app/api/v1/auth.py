422de86f6204748478f58cd806218c1e711f5a31
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 13:16:51 -0400

    subiendo auth

--- part3.1/app/api/v1/auth.py
+++ part3.1/app/api/v1/auth.py
@@ -5,31 +5,29 @@ from app.services import facade
 api = Namespace('auth', description='Authentication operations')
 
 login_model = api.model('Login', {
-  'email': fields.String(required=True, description='User email'),
-  'password': fields.String(required=True, description='User password')
+    'email': fields.String(required=True, description='User email'),
+    'password': fields.String(required=True, description='User password')
 })
+
 
 @api.route('/login')
 class Login(Resource):
-  @api.expect(login_model)
-  def post(self):
-    credentials = api.payload
-    
-    user = facade.get_user_by_email(credentials['email'])
-    
-    if not user or not user.verify_password(credentials['password']):
-      return {'error': 'Invalid credentials'}, 401
-    
-    print(f"is_admin for user {user.id}: {user.is_admin}")
+    @api.expect(login_model)
+    def post(self):
+        credentials = api.payload
+        user = facade.get_user_by_email(credentials['email'])
 
-    access_token = create_access_token(identity={'id': str(user.id), 'is_admin': user.is_admin})
-    
-    return {'access_token': access_token}, 200
-  
-@api.route('/protected')
-class ProtectedResource(Resource):
-  @jwt_required()
-  def get(self):
-    current_user_id = get_jwt_identity()
-    return {'message': f'Hello, user {current_user_id}'}, 200
-  
+        if not user or not user.verify_password(credentials['password']):
+            return {'error': 'Invalid credentials'}, 401
+
+        access_token = create_access_token(identity={'id': str(user.id), 'is_admin': user.is_admin})
+
+        return {'access_token': access_token}, 200
+
+    @api.route('/protected')
+    class ProtectedResource(Resource):
+        @jwt_required()
+        def get(self):
+            current_user = get_jwt_identity()
+            user_id = current_user.get("id") if isinstance(current_user, dict) else "unknown"
+            return {'message': f'Hello, user {user_id}'}, 200
