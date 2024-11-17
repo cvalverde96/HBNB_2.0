3ff8034f0d2c371432050b41e17ec1e82f4308a9
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 12:32:34 -0400

    subiendo config

--- part3.1/config.py
+++ part3.1/config.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
 import os
 
-class Config():
-    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
-
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
 config = {
-    "development": DevelopmentConfig,
-    "default": DevelopmentConfig
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
 }
