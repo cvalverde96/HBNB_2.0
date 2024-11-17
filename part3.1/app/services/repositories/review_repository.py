4193703cd5114af5dea75cd7f03bd8a7816b3462
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 14:52:29 -0400

    subiendo review

---
+++ part3.1/app/services/repositories/review_repository.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from app.models.review import Review
+from app.persistence.repository import SQLAlchemyRepository
+
+
+class ReviewRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Review)
