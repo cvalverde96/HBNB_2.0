d84d0e83d2159bf17c37fbc1c15288d3dabfb39a
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 14:04:46 -0400

    subiendo review

--- part3.1/app/models/review.py
+++ part3.1/app/models/review.py
@@ -1,50 +1,35 @@
 #!/usr/bin/python3
 
 from app.models.base import BaseModel
-from app.models.place import Place
-from app.models.user import User
+from app import db
 
 
 class Review(BaseModel):
-    def __init__(self, text: str, rating: int, place: "Place", user: "User"):
+    __tablename__ = 'review'
+    __table_args__ = {'extend_existing': True}
+
+    text = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
+
+    def __init__(self, text, rating, place_id, user_id):
         super().__init__()
-        self.text = self.validate_text(text)
-        self.rating = self.validate_rating(rating)
-        self.place = self.validate_place(place)
-        self.user = self.validate_user(user)
-    
-    @staticmethod
-    def validate_text(text: str) -> str:
-        if not text:
-            raise ValueError("Text required")
-        return (text)
-    
-    @staticmethod
-    def validate_rating(rating: int) -> int:
-        if not (1 <= rating <= 5):
-            raise ValueError("Rating given to the place, must be between 1 and 5.")
-        return (rating)
-    
-    @staticmethod
-    def validate_place(place: "Place") -> "Place":
-        if not isinstance(place, Place):
-            raise ValueError("Must be a valid Place instance")
-        return (place)
-    
-    @staticmethod
-    def validate_user(user: "User") -> "User":
-        if not isinstance(user, User):
-            raise ValueError("Must be a valid User instance")
-        return (user)
-    
-    def update(self, text: str = None, rating: int = None):
-        if text:
-            self.text = self.validate_text(text)
-        if rating:
-            self.rating= self.validate_rating(rating)
-        super().update()
-    
-    def __str__(self):
-        return (f"Review(id={self.id}, text={self.text}, rating={self.rating}, "
-                f"place={self.place.title}, user={self.user.first_name} {self.user.last_name}, "
-                f"created_at={self.created_at}, updated_at={self.updated_at})")
+        self.text = text
+        self.rating = rating
+        self.place_id = place_id
+        self.user_id = user_id
+        self.validations()
+
+    def validations(self):
+        if self.rating < 0 or self.rating > 5:
+            raise ValueError("Rating must be between 0 and 5")
+
+        if self.text is None:
+            raise ValueError("Review required")
+
+        if not isinstance(self.place_id, str) or not self.place_id:
+            raise ValueError("Place ID must be valid.")
+
+        if not isinstance(self.user_id, str) or not self.user_id:
+            raise ValueError("User ID must be valid.")
