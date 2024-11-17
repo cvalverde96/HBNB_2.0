d8699142fe2657f2502161a76eb63706603ed4d9
cvalverde96 <cvalverde96@gmail.com>
2024-11-17 13:06:14 -0400

    subiendo init file

--- part3.1/app/__init__.py
+++ part3.1/app/__init__.py
@@ -17,22 +17,40 @@ from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.admin import api as admin_ns
+from app.api.v1.placeamenities import api as placeamenities_ns
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
-    
-    api = Api(app, version="1.0", title="HBnB API", description="HBnB Application API")
+
+    authorizations = {
+        "BearerAuth": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+        }
+    }
+
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        authorizations=authorizations,
+        security="BearerAuth",
+    )
+
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(admin_ns, path='/api/v1/admin')
+    api.add_namespace(placeamenities_ns, path='/api/v1/placeamenities')
 
     return (app)
