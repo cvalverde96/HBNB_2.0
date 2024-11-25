1fd05305a32ccbcc5e538cf9be49e139a7b6c320
cvalverde96 <cvalverde96@gmail.com>
2024-11-25 09:49:43 -0400

    subiendo script con event listener de login form

---
+++ part3.1/base_files/scripts.js
@@ -0,0 +1,15 @@
+/* 
+  This is a SAMPLE FILE to get you started.
+  Please, follow the project instructions to complete the tasks.
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      console.log('Login form submitted!')
+    });
+  }
+});
